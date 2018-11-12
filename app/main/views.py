12e763ea6dbb04e91106cafaca61f53c9a69aff2
aluoch-sheila <aluochsheila1999@gmail.com>
2018-11-12 11:36:51 +0300

    fixed gitignore

---
+++ app/main/views.py
@@ -0,0 +1,53 @@
+#Imports
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..request import get_news,get_headlines
+from ..models import News,Headlines
+
+#views
+
+@main.route('/')
+def index():
+   """
+   View root page function that returns the index page and its data
+   """
+   #   getting news
+   business_news=get_news('business')
+   entertainment_news=get_news('entertainement')
+   general_news=get_news('general')
+   health_news=get_news('health')
+   science_news=get_news('science')
+   sports_news=get_news('sports')
+   technology_news=get_news('technology')
+
+   title = 'Home - Top'
+
+
+   search_news = request.args.get('search_query')
+
+   if search_news:
+       return redirect(url_for('search',news_name=search_news))
+   else:
+       return render_template('index.html', title = title, general = general_news, entertainment = entertainment_news,health = health_news,sports = sports_news,science = science_news,technology = technology_news,business = business_news )
+
+@main.route('/search/<news_name>')
+def search(news_name):
+   '''
+   View function to display the search results
+   '''
+   news_name_list = news_name.split(" ")
+   news_name_format = "+".join(news_name_list)
+   searched_news = search_news(news_name_format)
+   title = f'search results for {news_name}'
+   return render_template('search.html',news = searched_news)
+
+
+@main.route('/headlines/<id>')
+def headlines(id):
+   """
+   View headlinse from a specific source
+   """
+   headlines = get_headlines(id)
+   title = f'{id}'
+
+   return render_template('news.html',title = title,headlines = headlines)
---
+++ app/models.py
@@ -0,0 +1,24 @@
+class News:
+ """
+ News class to define news source objects
+ """
+
+ def __init__(self,id,name,url,category,description):
+     self.id = id
+     self.name = name
+     self.url = url
+     self.category = category
+     self.description = description
+
+class Headlines:
+ """
+ Headlines class to define news headlines objects
+ """
+
+ def __init__(self,id,title,description,url,urlToImage,publishedAt):
+     self.id = id
+     self.title = title
+     self.description = description
+     self.url = url
+     self.urlToImage = urlToImage
+     self.publishedAt = publishedAt
